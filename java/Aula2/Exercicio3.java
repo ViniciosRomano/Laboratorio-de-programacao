c6b57e11cd3cf431c76d3e453f16d8acd3bf8da0
Vinicios Romano Araujo <82785888+ViniciosRomano@users.noreply.github.com>
2022-04-13 09:41:20 -0300

    Add files via upload

---
+++ java/Aula2/Exercicio3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Exercicio3 {
+
+	public static void main(String[] args) {
+		Scanner i;
+		i = new Scanner(System.in);	
+		float A,B,C;
+		System.out.println("Digite um valor");
+		A = i.nextFloat();
+		System.out.println("Digite outro valor");
+		B = i.nextFloat();
+		System.out.println("Digite outro valor");
+		C = i.nextFloat();
+		switch()
+	}
+
+}
