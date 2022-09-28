a7cfa8c59095ad6156e729481cdf0fed1c1cf3e2
ViniciosRomano <zextremevini@hotmail.com>
2022-09-28 09:04:59 -0300

    Up

--- Java/String0928/src/Main.java
+++ Java/String0928/src/Main.java
@@ -1,8 +1,23 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        String nomeINN;
+        String nomeOUT = "";
+        String nomes[] = new String[1];
+        int i;
 
+        System.out.println("Digite seu nome: ");
+        nomeINN = input.nextLine().toUpperCase(Locale.ROOT).trim();
+        //Remover Espaço em branco .trim()
+        nomes =  nomeINN.split(" ");
+        nomeOUT = nomeOUT + nomes[nomes.length-1]+", ";
+        for (i=0;i< nomes.length-1;i++){
+            nomeOUT = nomeOUT + nomes[i].charAt(0)+". ";
+        }
+        System.out.println("NomeINN: "+nomeINN);
+        System.out.println("NomeOut: "+nomeOUT);
     }
 }
