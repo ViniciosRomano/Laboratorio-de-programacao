cca6c2de8327262de061caa1848c92a1496736a9
ViniciosRomano <zextremevini@hotmail.com>
2022-10-20 15:03:28 -0300

    Up

---
+++ JavaCodes/All Files IC/ApiVisionFile/src/getApiFile.java
@@ -0,0 +1,77 @@
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.FileEntity;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.math.RoundingMode;
+import java.net.URI;
+import java.text.DecimalFormat;
+
+public class getApiFile {
+    public static String result() {
+                String allResult;
+
+                HttpClient httpclient = HttpClients.createDefault();
+                allResult = "";
+                try
+                {
+                    URIBuilder builder = new URIBuilder("https://desenvolvimento-prediction.cognitiveservices.azure.com/customvision/v3.0/Prediction/0cae8647-5e8f-4b3e-8293-31191a0c3c3c/classify/iterations/Iteration4/image");
+
+                    URI uri = builder.build();
+                    HttpPost request = new HttpPost(uri);
+                    request.setHeader("Content-Type", "application/octet-stream");
+                    request.setHeader("Prediction-key", "75ab507a8d6e43d5a6ff7eeb17c522e1");
+
+
+                    File file = new File("C:\\Users\\zExtr\\Desktop\\Exibir\\Queimadura99%.jpg");
+                    FileEntity reqEntity = new FileEntity(file);
+                    request.setEntity(reqEntity);
+
+                    HttpResponse response = httpclient.execute(request);
+                    HttpEntity entity = response.getEntity();
+
+                    if (entity != null)
+                    {
+                        allResult = EntityUtils.toString(entity);
+
+                    }
+                }
+                catch (Exception e)
+                {
+                    allResult = e.getMessage();
+                }
+                return allResult;
+    }
+
+    public static String filter(){
+        String finalResult = "";
+
+        DecimalFormat df = new DecimalFormat("#.##%");
+        df.setRoundingMode(RoundingMode.DOWN);
+        String allResult []= new String[4];
+
+        String jsonString = getApiFile.result();
+        JSONObject obj = new JSONObject(jsonString);
+        JSONArray arr = obj.getJSONArray("predictions");
+        for (int i = 0; i < arr.length(); i++)
+        {
+            String tagName = arr.getJSONObject(i).getString("tagName") + ": ";
+            String probability = df.format(arr.getJSONObject(i).getDouble("probability"));
+            allResult[i] = tagName.concat(probability);
+        }
+        for (int i = 0; i < arr.length(); i++)
+        {
+            finalResult = finalResult.concat(allResult[i] + "\n");
+        }
+        return finalResult;
+
+    }
+    }
+
--- JavaCodes/All Files IC/ApiVisionFile/src/icApiFile.java
+++ JavaCodes/All Files IC/ApiVisionFile/src/icApiFile.java
@@ -1,50 +1,7 @@
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.utils.URIBuilder;
-import org.apache.http.entity.FileEntity;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
-
-import java.io.File;
-import java.net.URI;
-
 
 public class icApiFile {
     public static void main(String[] args) {
-        // // This sample uses the Apache HTTP client from HTTP Components (http://hc.apache.org/httpcomponents-client-ga/)
-                HttpClient httpclient = HttpClients.createDefault();
-
-                try
-                {
-                    URIBuilder builder = new URIBuilder("https://desenvolvimento-prediction.cognitiveservices.azure.com/customvision/v3.0/Prediction/0cae8647-5e8f-4b3e-8293-31191a0c3c3c/classify/iterations/Iteration4/image");
-
-                   // builder.setParameter("application", "{string}");
-
-                    URI uri = builder.build();
-                    HttpPost request = new HttpPost(uri);
-                    request.setHeader("Content-Type", "application/octet-stream");
-                    request.setHeader("Prediction-key", "75ab507a8d6e43d5a6ff7eeb17c522e1");
-
-
-                    // Request body
-                    File file = new File("C:\\Users\\zExtr\\Desktop\\Exibir\\Ulcera99%.png");
-                    FileEntity reqEntity = new FileEntity(file);
-                    request.setEntity(reqEntity);
-
-                    HttpResponse response = httpclient.execute(request);
-                    HttpEntity entity = response.getEntity();
-
-                    if (entity != null)
-                    {
-                        System.out.println(EntityUtils.toString(entity));
-                    }
-                }
-                catch (Exception e)
-                {
-                    System.out.println(e.getMessage());
-                }
-            }
-        }
+                System.out.println(getApiFile.filter());
+    }
+}
 
